f1928078cb7b4ca00a96e9e9826eb6a0c881feef
ybkl0 <ybkl0@DESKTOP-7JSNST4>
2021-07-05 04:27:21 +0900

    07-04

--- 20210630/src/co/yedam/exercise20/BackApplication.java
+++ 20210630/src/co/yedam/exercise20/BackApplication.java
@@ -30,31 +30,46 @@ public class BackApplication {
 		}
 		System.out.println("프로그램 종료");
 	} // end of main()
-	
-	//계좌생성하기
+
+	// 계좌생성하기
 	private static void createAccount() {
 		System.out.println("-------");
 		System.out.println("계좌생성");
 		System.out.println("-------");
-		
+
+		System.out.print("계좌번호: ");
+		String account = scanner.nextLine();
+		System.out.print("계좌주: ");
+		String name = scanner.nextLine();
+		System.out.print("초기입금액: ");
+		int amount = scanner.nextInt();
+		System.out.println("계좌가 생성되었습니다.");
+
+		for (int i = 0; i < accountArray.length; i++) {
+			if (accountArray[i] == null) {
+				Account ac = new Account(account, name, amount);
+				accountArray[i] = ac;
+				break;
+			}
+		}
 	}
-	
-	//계좌목록보기
+
+	// 계좌목록보기
 	private static void accountList() {
-		
+
 	}
-	
-	//예금하기
+
+	// 예금하기
 	private static void deposit() {
-		
+
 	}
-	
-	//출금하기
+
+	// 출금하기
 	private static void withdraw() {
-		
+
 	}
-	
-	//Account 배열에서 ano와 동일한 Account 객체 찾기
+
+	// Account 배열에서 ano와 동일한 Account 객체 찾기
 	private static Account findAccount(String ano) {
 		return null;
 	}
